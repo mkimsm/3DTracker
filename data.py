0a1e09619e017c88685359517bd9f4a150dc95f6
ssteeper <ssteeper@calpoly.edu>
2012-05-16 14:34:45 -0700

    Some notes for readability

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def stringtoarray(s):
+   #Remove the return and split the string into a list of chars
    s = s[:len(s)-1]
    s = s.split(' ')
 
+   #Convert each element of the list into integers, convert to numpy array
    i=0
    for num in s:
       s[i] = int(s[i])
@@ -17,11 +19,11 @@ def package(x):
    pkg = []
    L = -1
    for line in f:
-      if line[0] == '#':
+      if line[0] == '#': #Create a new list for a new event
          a = []
          pkg.append(a)
          L += 1
       else:
-         pkg[L].append(stringtoarray(line))
+         pkg[L].append(stringtoarray(line)) #Append the array version of the string to the current list
    return pkg
 
