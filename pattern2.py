341a5ce444d9dbdc1f09aa56dbb276f633442dd0
Mike Kim <mkim20@calpoly.edu>
2012-05-28 12:16:38 -0700

    starting mark

---
+++ pattern2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# What the code needs
+# Weighted Least Squares
+# Gradient
+# Maybe's
+# Distance
+# Find Points
+d = math.sqrt(x^2+y^2+z^2);
+def pattern(step,x,y,z,q):
+	return "x,y,z,q"
+
+	
